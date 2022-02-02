2c27cc51734e5c61d4dcf4c9e9decb728866d572
sehraf <sehraf42@gmail.com>
2022-02-02 21:07:30 +0100

    Rebase on master  - port RGB matrix effects to "enable" scheme  - clang-format

--- keyboards/sofle/keymaps/crtified/config.h
+++ keyboards/sofle/keymaps/crtified/config.h
@@ -1,35 +1,35 @@
- /* Copyright 2021 Dane Evans
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 2 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-  #pragma once
+/* Copyright 2021 Dane Evans
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
 
 /* Select hand configuration */
 
-///https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+/// https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
 
 #define MASTER_LEFT
 
 #define CUSTOM_FONT
 
-#define CUSTOM_LAYER_READ //if you remove this it causes issues - needs better guarding
+#define CUSTOM_LAYER_READ  // if you remove this it causes issues - needs better guarding
 
 
 #define TAPPING_FORCE_HOLD
 #ifdef TAPPING_TERM
-    #undef TAPPING_TERM
-    #define TAPPING_TERM 200
+#    undef TAPPING_TERM
+#    define TAPPING_TERM 200
 #endif
 #define ENCODER_DIRECTION_FLIP
 
@@ -41,50 +41,72 @@
 #define RGB_DI_PIN D3
 
 #ifdef RGB_MATRIX_ENABLE
-#   define RGBLED_NUM 72
-#   define DRIVER_LED_TOTAL RGBLED_NUM
-#   define RGB_MATRIX_SPLIT { 36, 36 }
+#    define RGBLED_NUM 72
+#    define DRIVER_LED_TOTAL RGBLED_NUM
+#    define RGB_MATRIX_SPLIT \
+        { 36, 36 }
 
-#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-#   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#    define RGB_MATRIX_KEYPRESSES           // reacts to keypresses
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED  // turn off effects when suspended
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120  // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
 
-#   define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_CROSS
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_CROSS
 
-#   define RGB_MATRIX_HUE_STEP 8
-#   define RGB_MATRIX_SAT_STEP 8
-#   define RGB_MATRIX_VAL_STEP 8
-#   define RGB_MATRIX_SPD_STEP 10
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
 
 /* Disable the animations you don't want/need.  You will need to disable a good number of these    *
  * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
-//#   define DISABLE_RGB_MATRIX_ALPHAS_MODS
-#   define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-#   define DISABLE_RGB_MATRIX_BREATHING
-#   define DISABLE_RGB_MATRIX_CYCLE_ALL
-#   define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#   define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
-#   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
-#   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-#   define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-#   define DISABLE_RGB_MATRIX_DUAL_BEACON
-#   define DISABLE_RGB_MATRIX_RAINBOW_BEACON
-#   define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-#   define DISABLE_RGB_MATRIX_RAINDROPS
-#   define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-#   define DISABLE_RGB_MATRIX_TYPING_HEATMAP
-#   define DISABLE_RGB_MATRIX_DIGITAL_RAIN
-#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE
-#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-//#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#   define DISABLE_RGB_MATRIX_SPLASH
-#   define DISABLE_RGB_MATRIX_MULTISPLASH
-#   define DISABLE_RGB_MATRIX_SOLID_SPLASH
-#   define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
-#endif
+#    define ENABLE_RGB_MATRIX_ALPHAS_MODS
+// #    define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+// #    define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+// #    define ENABLE_RGB_MATRIX_BREATHING
+// #    define ENABLE_RGB_MATRIX_BAND_SAT
+// #    define ENABLE_RGB_MATRIX_BAND_VAL
+// #    define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+// #    define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+// #    define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+// #    define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+// #    define ENABLE_RGB_MATRIX_CYCLE_ALL
+// #    define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+// #    define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+// #    define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+// #    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+// #    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #    define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+// #    define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+// #    define ENABLE_RGB_MATRIX_DUAL_BEACON
+// #    define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+// #    define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+// #    define ENABLE_RGB_MATRIX_RAINDROPS
+// #    define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+// #    define ENABLE_RGB_MATRIX_HUE_BREATHING
+// #    define ENABLE_RGB_MATRIX_HUE_PENDULUM
+// #    define ENABLE_RGB_MATRIX_HUE_WAVE
+// #    define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+// #    define ENABLE_RGB_MATRIX_PIXEL_RAIN
+
+#    ifdef RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #        define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+// #        define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#    endif  // RGB_MATRIX_FRAMEBUFFER_EFFECTS
+
+#    if defined(RGB_MATRIX_KEYPRESSES) || defined(RGB_MATRIX_KEYRELEASES)
+// #        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #        define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+// #        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// #        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// #        define ENABLE_RGB_MATRIX_SPLASH
+// #        define ENABLE_RGB_MATRIX_MULTISPLASH
+// #        define ENABLE_RGB_MATRIX_SOLID_SPLASH
+// #        define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#    endif  // KEYPRESS / KEYRELEASE
+
+#endif  // RGB_MATRIX_ENABLE
